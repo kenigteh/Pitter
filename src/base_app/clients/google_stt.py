ec51ee2b334ed2e9cc9c09c754ce2203fccf5d00
Artem <34108059+buispro@users.noreply.github.com>
2019-04-08 22:15:36 +0200

    Speech to text work now

--- src/base_app/clients/google_stt.py
+++ src/base_app/clients/google_stt.py
@@ -2,6 +2,9 @@ import base64
 
 import json
 import requests
+from base_app import constants
+
+REQ_DATA = constants.REQ_DATA
 
 
 def mp3_to_flac(audio):
@@ -10,35 +13,33 @@ def mp3_to_flac(audio):
 
 def encode_audio(audio):
     audio_content = audio.read()
-    return base64.b64encode(audio_content).decode('ascii')
+    return base64.b64encode(audio_content).decode("ascii")
 
 
 def create_rec(audio_content):
     data = {
         'config': {
-            'encoding': 'FLAC',
-            'sampleRateHertz': 16000,
-            'languageCode': 'ru-RU'
+            'encoding': REQ_DATA["encoding"],
+            'languageCode': REQ_DATA["languageCode"]
         },
         'audio': {
-            'content': str(audio_content)
+            'content': audio_content
         }
     }
     return data
 
 
 def send_req(data):
-    key = 'AIzaSyAPgaSnLgDd0tIP_YxfKXBf59fNQ-IKocA'
-    url = f"https://speech.googleapis.com/v1/speech:recognize?alt=json&key={key}"
-    return requests.post(url, data=json.dumps(data)).json()
+    return requests.post(constants.url, json=data, params={"key": constants.key}).text
 
 
-def speech_to_text(audio):
-    data = encode_audio(audio)
+def speech_to_text(audio_path):
+    data = encode_audio(audio_path)
     req_data = create_rec(data)
     print(send_req(req_data))
 
 
-file = open("test.flac", "rb")
-speech_to_text(file)
-file.close()
+f = open("qwerty.flac", "rb")
+speech_to_text(f)
+
+f.close()
