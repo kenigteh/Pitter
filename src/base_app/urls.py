2462aec19625784bba2165ff2ee5a893179d6052
kenigteh <artemon1002@gmail.com>
2019-04-10 23:18:23 +0200

    Upgrade urls.py

--- src/base_app/urls.py
+++ src/base_app/urls.py
@@ -5,4 +5,5 @@ from base_app import views
 urlpatterns = [
     url('health', views.HeartBeatHealthCheck.as_view()),
     url('upload_audio', views.UploadAudioFile.as_view()),
+    url('user_manager', views.UserManager.as_view()),
 ]
--- src/base_app/views/__init__.py
+++ src/base_app/views/__init__.py
@@ -1,7 +1,9 @@
 from .healthcheck import HeartBeatHealthCheck
 from .upload_audio_file import UploadAudioFile
+from .user_manager import UserManager
 
 __all__ = [
     'HeartBeatHealthCheck',
     'UploadAudioFile',
+    'UserManager',
 ]
