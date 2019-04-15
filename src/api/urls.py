1c7ee0e38fce4b224642de7e501d0fd4163d63d9
Artem <34108059+buispro@users.noreply.github.com>
2019-04-15 12:04:03 +0200

    Delete stt

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url('upload_audio', views.UploadAudioFile.as_view()),
     url('user_manager', views.UserManager.as_view()),
     url('auth', views.Authorization.as_view()),
 ]
--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,9 +1,7 @@
-from .upload_audio_file import UploadAudioFile
 from .user_manager import UserManager
 from .auth import Authorization
 
 __all__ = [
-    'UploadAudioFile',
     'UserManager',
     'Authorization',
 ]
