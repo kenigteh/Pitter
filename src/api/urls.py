289f06a0296e260d8db43e18b229dac7d984daa7
Artem <34108059+buispro@users.noreply.github.com>
2019-04-17 18:50:32 +0200

    Create new view

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url('user_manager', views.UserManager.as_view()),
+    url('users/manager', views.UserManager.as_view()),
+    url('users/search', views.UserManager.as_view()),
     url('auth', views.Authorization.as_view()),
 ]
--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,7 +1,11 @@
 from .user_manager import UserManager
 from .auth import Authorization
+from .subscription_manager import SubManager
+from .users_search import UsersSearch
 
 __all__ = [
     'UserManager',
     'Authorization',
+    'SubManager',
+    'UsersSearch',
 ]
