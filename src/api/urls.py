8d64fc38e629c5d0c9b292875758720a973b349f
Artem <34108059+buispro@users.noreply.github.com>
2019-04-17 18:51:51 +0200

    Update urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,6 +4,7 @@ from api import views
 
 urlpatterns = [
     url('users/manager', views.UserManager.as_view()),
-    url('users/search', views.UserManager.as_view()),
+    url('users/search', views.UsersSearch.as_view()),
+    url('subscriptions', views.SubManager.as_view()),
     url('auth', views.Authorization.as_view()),
 ]
