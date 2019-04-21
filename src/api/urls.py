0a09d6d3064a87c4bcf03c1a403be703cf6af352
Artem <34108059+buispro@users.noreply.github.com>
2019-04-21 23:28:55 +0200

    Add pitts view

--- src/api/urls.py
+++ src/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url('users/search', views.UsersSearch.as_view()),
     url('users/subscriptions', views.SubManager.as_view()),
     url('users/auth', views.Authorization.as_view()),
+    url('pitts', views.Pitts.as_view()),
 ]
--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -2,10 +2,12 @@ from .user_manager import UserManager
 from .auth import Authorization
 from .subscription_manager import SubManager
 from .users_search import UsersSearch
+from .pitts import Pitts
 
 __all__ = [
     'UserManager',
     'Authorization',
     'SubManager',
     'UsersSearch',
+    'Pitts',
 ]
