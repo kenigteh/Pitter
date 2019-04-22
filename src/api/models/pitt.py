77ef96cf6f89ede5ff7c925f52b6d55f8f194122
Artem <34108059+buispro@users.noreply.github.com>
2019-04-22 11:05:45 +0200

    Create pitt upload func

--- src/api/models/pitt.py
+++ src/api/models/pitt.py
@@ -7,6 +7,6 @@ from django.db import models
 class Pitt(models.Model):
     pitt_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
     user_id = models.CharField(max_length=128)
-    audio_file_name = models.FilePathField(max_length=128)
+    audio_file = models.FileField(upload_to='api/audiofiles')
     audio_text = models.CharField(max_length=128)
     date = models.DateTimeField(default=datetime.now)
--- src/api/views/pitts.py
+++ src/api/views/pitts.py
@@ -1,5 +1,25 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from api.models import Pitt
+from api.decorators import token_validation, login_validation
 
 
 class Pitts(APIView):
-    pass
+    @staticmethod
+    @token_validation
+    @login_validation
+    def post(request):
+        audio_file = request.data.get('audio_file')
+        audio_text = request.data.get('audio_text')
+
+        if not audio_file or not audio_text or isinstance(audio_text, str):
+            data = dict(error="Bad request!")
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+        user_id = request.current_app.get('user_id')
+
+        Pitt.objects.create(user_id=user_id, audio_file=audio_file, audio_text=audio_text)
+        data = dict(status="Success!")
+        return Response(data=data, status=status.HTTP_201_CREATED)
