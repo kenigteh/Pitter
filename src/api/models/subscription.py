950f5f53fe1925254a6136442ea491933675027e
Artem <34108059+buispro@users.noreply.github.com>
2019-04-24 19:02:23 +0200

    Update subscription model with 'null' kward

--- src/api/models/subscription.py
+++ src/api/models/subscription.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Subscription(models.Model):
     sub_id = models.CharField(max_length=128, default=uuid.uuid4, primary_key=True)
-    user_from = models.CharField(max_length=128)
-    user_to = models.CharField(max_length=128)
+    user_from = models.CharField(max_length=128, null=False)
+    user_to = models.CharField(max_length=128, null=False)
