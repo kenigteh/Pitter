ec6637fcf48ad2ef03d0757941c3d33dd60b7341
Artem <34108059+buispro@users.noreply.github.com>
2019-04-25 12:24:36 +0200

    Update auth error catch

--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -18,23 +18,21 @@ class Authorization(APIView):
         return encoded_jwt
 
     def post(self, request):
-        print(1)
-        try:
-            login = request.data['login']
-            password = request.data['password']
-        except KeyError:
+        login = request.data.get('login', False)
+        password = request.data.get('password', False)
+        if not login or not password:
             data = dict(error="No user password or login!")
             return Response(data, status=400)
 
         try:
             user = User.objects.get(login=login, password=password)
         except ObjectDoesNotExist:
-            data = dict(error="User not found!")
-            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+            data = dict(error="Your login or password is incorrect")
+            return Response(data=data, status=status.HTTP_401_UNAUTHORIZED)
 
         if not user:
-            data = dict(error="Your login or password is incorrect")
-            return Response(data, status=401)
+            data = dict(error="User not found!")
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
         data = dict(key=self.create_jwt(login))
         return Response(data, status=200)
